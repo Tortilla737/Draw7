475e6ba0a42a15bcea75cffcb9fb10b2559f4a6c
Gerrit <gary-schnecke@gmx.de>
2018-12-18 00:37:10 +0100

    Added DeckHandler + TextInputHandler

---
+++ Assets/Scripts/DeckHandlerSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckHandlerSystem : MonoBehaviour {
+
+    public Text textField;
+    public Text nameField;
+    private string[] deckList;
+
+
+	void Start () {
+		
+	}
+
+    public void TextToList()
+    {
+        char[] delimiters = new char[] { '\r', '\n' };
+        deckList = textField.text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string i in deckList)
+        {
+            Debug.Log(i);
+
+        }
+    }
+
+    public void SaveStringToText(string stringInput, string deckName)
+    {
+        string path = Application.dataPath + "/DecklistData/" + deckName + ".txt";
+
+        StreamWriter writer = new StreamWriter(path);
+        if (File.Exists(path))
+        {
+            writer.AutoFlush = true;
+            writer.WriteLine(stringInput);
+            
+        }
+        else
+        {
+            File.Create(path);
+            SaveStringToText(stringInput, deckName);
+        }
+        writer.Close();
+    }
+
+    public void LoadDecklist(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            StreamReader reader = new StreamReader(filePath);
+
+            for (int i = 0; i<= deckList.Length; i++)
+            {
+
+            }
+            string job = reader.ReadLine();
+
+            reader.Close();
+        }
+        else
+        {
+            Debug.Log(filePath + " nicht gefunden");
+        }
+    }
+}
---
+++ Assets/Scripts/TextInputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInputHandler : MonoBehaviour {
+    
+    
+    private TouchScreenKeyboard keyboard;
+    private bool writeEnabled = false;
+
+    // Updates button's text while user is typing
+    void OnGUI()
+    {
+        if (writeEnabled)
+        {
+            keyboard = TouchScreenKeyboard.Open(GetComponent<Text>().text, TouchScreenKeyboardType.Default, false, true, false);
+        }
+
+        if (keyboard != null)
+        {
+            GetComponent<Text>().text = keyboard.text;
+            writeEnabled = false;
+        }
+    }
+
+    public void StartEdit()
+    {
+        writeEnabled = true;
+        
+    }
+}
