cd49c44008ab8dd1b6c93fc6e99b6c04dd47ed95
Gerrit <gary-schnecke@gmx.de>
2018-12-17 15:07:40 +0100

    Added Icons, Panels and leftMenu

---
+++ Assets/Scripts/LayoutHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LayoutHandler : MonoBehaviour {
+
+    public GameObject leftPanel;
+    private Animator menuAnimator;
+    private bool menuIsOpen;
+
+    private void Awake()
+    {
+        menuAnimator = leftPanel.GetComponent<Animator>();
+        menuIsOpen = false;
+    }
+
+    public void animateMenu(){
+        menuIsOpen = !menuIsOpen;
+        menuAnimator.SetBool("openMenu", menuIsOpen);
+    }
+
+}
