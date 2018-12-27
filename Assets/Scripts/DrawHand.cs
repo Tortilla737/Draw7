e9f6151426197a4a50f1eeacdc486384de16f6b0
Gerrit <gary-schnecke@gmx.de>
2018-12-27 02:53:26 +0100

    Set Card Layout on Draw page + finfished draw script

--- Assets/Scripts/DrawHand.cs
+++ Assets/Scripts/DrawHand.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DrawHand : MonoBehaviour {
+public class DrawHand : MonoBehaviour
+{
 
     private List<string> deckList = new List<string>();
     public bool pictureMode;
@@ -14,27 +15,37 @@ public class DrawHand : MonoBehaviour {
 
     private void Start()
     {
-        
+
     }
 
     public void DrawHandButton()
     {
         GetDecklist();
         GetComponent<SceneHandler>().NewhandScene();
-        MakeRandom();
+        foreach (GameObject i in cardPanels)
+        {
+            RandomCardInPanel(i);
+        }
     }
 
     public void RedrawButton()
     {
-
-    }
-
-    private void MakeRandom()
-    {
         foreach (GameObject i in cardPanels)
         {
-
+            RandomCardInPanel(i);
         }
+    }
+
+    private void RandomCardInPanel(GameObject cardPanel)
+    {
+        int[] deckIndex = new int[deckList.Count];
+        for(int i = 0; i< deckList.Count; i++) //für jede Karte gibt es einen prüf-Index, um Duplikate zu vermeiden
+        {
+            deckIndex[i] = i;
+        }
+        int randomInt = UnityEngine.Random.Range(0, deckList.Count);
+        // karte muss eingetragen werden
+        cardPanel.GetComponentInChildren<Text>().text = deckList[randomInt];
     }
 
     private void GetDecklist()
@@ -44,35 +55,35 @@ public class DrawHand : MonoBehaviour {
         char[] delimiters = new char[] { '\r', '\n' };
         string[] deckListBuffer = textField.text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-        char[] trimsStart = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', ' '};
+        char[] trimsStart = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', ' ' };
         foreach (string i in deckListBuffer)
         {
+            int cardCount;
+            int.TryParse(Regex.Replace(i, "[^0-9]", ""), out cardCount);
             string cardName = i.TrimStart(trimsStart);
             cardName.TrimEnd(' ');
-            deckList.Add(cardName);
-            Debug.Log(cardName);
-            /*
-            bool withPre = false;
-            foreach (char c in trimsStart)
+
+            if (cardName.EndsWith(")")) // Set Kürzel entfernen
             {
-                if (i.StartsWith(c.ToString()))
+                cardName = cardName.Substring(0, cardName.Length - 6);
+            }
+
+            if (cardCount == 0) // falls keine nummer vor der Karte steht wird sie einmal hinzugefügt
+            {
+                cardCount++;
+            }
+
+            for (int n = 0; n < cardCount; n++)
+            {
+                if (cardName.EndsWith("*"))
                 {
-                    withPre = true;
+                    //der Commander wird nicht ins Deck gemischt
+                }
+                else
+                {
+                    deckList.Add(cardName);
                 }
             }
-            if (withPre)
-            {
-                string cardName = i.TrimStart(trimsStart);
-                cardName.TrimEnd(' ');
-                deckList.Add(cardName);
-            }
-            else
-            {
-                deckList.Add(i.TrimEnd(' '));
-            }
-            */
         }
-
-        //deck sortieren und syntax fixen
     }
 }
--- Assets/Scripts/ImageGaleryHandler.cs
+++ Assets/Scripts/ImageGaleryHandler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.IO;
+using System;
 
 public class ImageGaleryHandler : MonoBehaviour {
 
@@ -51,5 +53,82 @@ public class ImageGaleryHandler : MonoBehaviour {
             Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
             //bufferImage.sprite = myTexture;
         }
+    }
+
+    //----------online lösung-------------
+
+    public void DownloadImage(string url, string pathToSaveImage)
+    {
+        WWW www = new WWW(url);
+        StartCoroutine(_downloadImage(www, pathToSaveImage));
+    }
+
+    private IEnumerator _downloadImage(WWW www, string savePath)
+    {
+        yield return www;
+
+        //Check if we failed to send
+        if (string.IsNullOrEmpty(www.error))
+        {
+            UnityEngine.Debug.Log("Success");
+
+            //Save Image
+            saveImage(savePath, www.bytes);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Error: " + www.error);
+        }
+    }
+
+    void SaveImage(string path, byte[] imageBytes)
+    {
+        //Create Directory if it does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+        }
+
+        try
+        {
+            File.WriteAllBytes(path, imageBytes);
+            Debug.Log("Saved Data to: " + path.Replace("/", "\\"));
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Failed To Save Data to: " + path.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
+        }
+    }
+
+    byte[] LoadImage(string path)
+    {
+        byte[] dataByte = null;
+
+        //Exit if Directory or File does not exist
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Debug.LogWarning("Directory does not exist");
+            return null;
+        }
+
+        if (!File.Exists(path))
+        {
+            Debug.Log("File does not exist");
+            return null;
+        }
+
+        try
+        {
+            dataByte = File.ReadAllBytes(path);
+            Debug.Log("Loaded Data from: " + path.Replace("/", "\\"));
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Failed To Load Data from: " + path.Replace("/", "\\"));
+            Debug.LogWarning("Error: " + e.Message);
+        }
+
+        return dataByte;
     }
 }
